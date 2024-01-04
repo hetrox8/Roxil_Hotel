fc5cc98cf3c3b79e0728860006fef4e8c3d37168
suleiman gacheru <dretrevor8@gmail.com>
2024-01-04 20:44:54 +0300

    add

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,7 +5,15 @@ const About = () => {
     <>
     <div className="abt-container">
       <div className="abt-1">
-        
+        <div className="abt-card">
+         <h2> About us</h2>
+         <h3> Roxil Hotel</h3>
+         <h4> escape to paradise: experience tranquility at roxil hotel</h4>
+         <p className='abt-p'></p>
+        </div>
+        <div className="abt-card">
+
+        </div>
       </div>
     </div>
     </>
