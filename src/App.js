735a8e2035fcc7ab2be200d62e7279e8273959d6
suleiman gacheru <dretrevor8@gmail.com>
2024-01-05 11:55:39 +0300

    fix imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
+import Nav from './components/Nav';
+import Home from './components/Home';
 import ChoicesForm from './components/SelectionBarComponents/ChoicesBar';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Nav from './Components/Nav';
-import Home from './Components/Home';
 
 const App = () => {
   return (
